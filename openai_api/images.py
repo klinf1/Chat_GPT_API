7f594b791379907626e1d95e411eb17200751e9f
Михаил Манузин <klinf157@gmail.com>
2023-04-03 19:23:35 +0300

    added logging for images

--- openai_api/images.py
+++ openai_api/images.py
@@ -6,15 +6,21 @@ logger = logging.getLogger('logger')
 
 
 def get_image(update, context):
-    prompt = ' '.join(context.args)
-    response = openai.Image.create(
-        prompt=prompt,
-        n=1,
-        size='1024x1024',
-        response_format='url'
-    )
-    context.bot.send_photo(
-        chat_id=update.effective_chat.id,
-        photo=response['data'][0]['url'],
-        caption=prompt
-    )
+    try:
+        prompt = ' '.join(context.args)
+        response = openai.Image.create(
+            prompt=prompt,
+            n=1,
+            size='1024x1024',
+            response_format='url'
+        )
+        context.bot.send_photo(
+            chat_id=update.effective_chat.id,
+            photo=response['data'][0]['url'],
+            caption=prompt
+        )
+        logger.info(
+            f'image {prompt} generated for user {update.effective_chat.id}'
+        )
+    except Exception as error:
+        logger.error(error)
